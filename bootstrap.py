67c18ae7fdbf855c6efd4a5caeab6c81af8ea844
Hong Quan Nguyen <quannguyen2281995@gmail.com>
2023-08-03 00:24:29 +0200

    finished bootstraping, config, test

--- bootstrap.py
+++ bootstrap.py
@@ -3,6 +3,7 @@ import time
 import tomllib
 import logging
 import argparse
+import threading
 import subprocess
 from typing import Dict
 
@@ -17,34 +18,39 @@ class CommandExecutor:
         self.verbose = verbose
         self.dry_run = dry_run
 
-    def execute(self, args, cwd: str = None, input: str = None, env: Dict[str, str] = None, background: bool = False):
+    def execute(self, args, cwd: str = None, env: Dict[str, str] = None):
         logging.info(f"Executing command:"
                      f"\ncwd = '{cwd}'"
-                     f"\ninput = '{input}'"
                      f"\nenv = {env}"
-                     f"\nbackground = '{background}'"
                      f"\ncommand = '{' '.join(args)}'"
                      "\n")
 
+        def read_stream(stream, callback):
+            for line in stream:
+                callback(line.decode().strip())
+
         if not self.dry_run:
             starttime = time.time()
-            if background:
-                proc = subprocess.Popen(args, cwd=cwd, env=env, stdin=subprocess.PIPE, start_new_session=True)
-                proc.stdin.write(input)
-                proc.stdin.flush()
-                proc.stdin.close()
-            else:
-                proc = subprocess.run(args, cwd=cwd, input=input, env=env, stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-                stdout = proc.stdout.decode("utf-8", "ignore")
-                stderr = proc.stderr.decode("utf-8", "ignore")
+            proc = subprocess.Popen(args, cwd=cwd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-                if stdout.strip():
-                    logging.info(f"Command stdout:\n{stdout}\n")
-                if stderr.strip():
-                    logging.info(f"Command stderr:\n{stderr}\n")
+            stdout_thread = threading.Thread(target=read_stream,
+                                             args=(proc.stdout, lambda line: logging.info(f"STDOUT: {line}")))
+            stderr_thread = threading.Thread(target=read_stream,
+                                             args=(proc.stderr, lambda line: logging.info(f"STDERR: {line}")))
 
-                logging.info(f"Command returncode={proc.returncode} , execution_time={time.time() - starttime}\n")
+            stdout_thread.start()
+            stderr_thread.start()
+
+            # Wait for the process to complete and threads to finish
+            proc.wait()
+            stdout_thread.join()
+            stderr_thread.join()
+
+            logging.info(f"Command returncode={proc.returncode} , execution_time={time.time() - starttime}\n")
+
+            return proc.returncode
+        else:
+            return 0
 
 
 class Config:
@@ -92,11 +98,15 @@ def deploy(cfg: Config, command_executor: CommandExecutor):
     args = [
         "terraform",
         "init",
-        "-auto-approve"
     ]
     # TODO: dynamic CWD, for now we only have aws+eks
     cwd = os.path.join(BASE_DIR, "terraform", "aws_eks")
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        if os.path.exists(os.path.join(cwd, '.terraform')):
+            logging.error("Failed to init terraform directory.")
+        else:
+            raise Exception("Failed to init terraform")
 
     args = [
         "terraform",
@@ -104,7 +114,9 @@ def deploy(cfg: Config, command_executor: CommandExecutor):
         "-auto-approve"
     ]
     cwd = os.path.join(BASE_DIR, "terraform", "aws_eks")
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        raise Exception("Failed to apply terraform manifest")
 
     # fetch kubeconfig
     args = [
@@ -119,25 +131,59 @@ def deploy(cfg: Config, command_executor: CommandExecutor):
         f"{os.path.join(BASE_DIR, 'kubeconfig')}",
     ]
     cwd = os.path.join(BASE_DIR)
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        if os.path.exists(os.path.join(BASE_DIR, 'kubeconfig')):
+            logging.warning(
+                f"Failed to create kubeconfig file, will reuse old file at {os.path.join(BASE_DIR, 'kubeconfig')}")
+        else:
+            raise Exception(f"Failed to create kubeconfig file at {os.path.join(BASE_DIR, 'kubeconfig')}")
 
-    # apply kubernetes
+    # deploy docker registry credential as secrets
+    dockerconfigjson = cfg.config['docker']['dockerconfigjson']
+    if dockerconfigjson and os.path.exists(dockerconfigjson):
+        args = [
+            "kubectl",
+            f"--kubeconfig={os.path.join(BASE_DIR, 'kubeconfig')}",
+            "create",
+            "secret",
+            "generic",
+            "regcred",
+            f"--from-file=.dockerconfigjson={cfg.config['docker']['dockerconfigjson']}",
+            f"--type=kubernetes.io/dockerconfigjson",
+        ]
+        cwd = os.path.join(BASE_DIR)
+        exit_code = command_executor.execute(args=args, cwd=cwd)
+        if exit_code != 0:
+            raise Exception(f"Failed to create docker registry credentials as Secrets")
+    else:
+        logging.warning(f"dockerconfigjson is not configured or not exists at path {dockerconfigjson}. "
+                        f"Make sure that the configured docker image can be pull without authentication required.")
+
     args = [
         "kubectl",
-        "config",
         f"--kubeconfig={os.path.join(BASE_DIR, 'kubeconfig')}",
-    ]
-    cwd = os.path.join(BASE_DIR)
-    command_executor.execute(args=args, cwd=cwd)
-
-    args = [
-        "kubectl",
         "apply",
         "-R",
         f"-f={os.path.join(BASE_DIR, 'kubernetes')}/"
     ]
     cwd = os.path.join(BASE_DIR, "kubernetes")
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        raise Exception("Failed to create kubernetes resources")
+
+    args = [
+        "kubectl",
+        f"--kubeconfig={os.path.join(BASE_DIR, 'kubeconfig')}",
+        "get",
+        "service",
+        "locust-master-public",
+        "--output=jsonpath=\"http://{.status.loadBalancer.ingress[0].hostname}:{.spec.ports[0].port}\""
+    ]
+    cwd = os.path.join(BASE_DIR, "kubernetes")
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        logging.error("Failed to fetch public URL for the locust Service")
 
 
 def destroy(cfg: Config, command_executor: CommandExecutor):
@@ -154,35 +200,50 @@ def destroy(cfg: Config, command_executor: CommandExecutor):
         f"{os.path.join(BASE_DIR, 'kubeconfig')}",
     ]
     cwd = os.path.join(BASE_DIR)
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        if os.path.exists(os.path.join(BASE_DIR, 'kubeconfig')):
+            logging.warning(
+                f"Failed to create kubeconfig file, will reuse old file at {os.path.join(BASE_DIR, 'kubeconfig')}")
+        else:
+            raise Exception(f"Failed to create kubeconfig file at {os.path.join(BASE_DIR, 'kubeconfig')}")
 
-    # apply kubernetes
     args = [
         "kubectl",
-        "config",
         f"--kubeconfig={os.path.join(BASE_DIR, 'kubeconfig')}",
-    ]
-    cwd = os.path.join(BASE_DIR)
-    command_executor.execute(args=args, cwd=cwd)
-
-    args = [
-        "kubectl",
         "delete",
         "-R",
         f"-f={os.path.join(BASE_DIR, 'kubernetes')}/"
     ]
     cwd = os.path.join(BASE_DIR, "kubernetes")
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        logging.error(f"Failed to delete kubernetes resources")
+
+    args = [
+        "kubectl",
+        f"--kubeconfig={os.path.join(BASE_DIR, 'kubeconfig')}",
+        "delete",
+        "secret/regcred",
+    ]
+    cwd = os.path.join(BASE_DIR, "kubernetes")
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        logging.error(f"Failed to delete docker registry credentials Secrets")
 
     # init and apply terraform
     args = [
         "terraform",
         "init",
-        "-auto-approve"
     ]
     # TODO: dynamic CWD, for now we only have aws+eks
     cwd = os.path.join(BASE_DIR, "terraform", "aws_eks")
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        if os.path.exists(os.path.join(cwd, '.terraform')):
+            logging.error("Failed to init terraform directory.")
+        else:
+            raise Exception("Failed to init terraform")
 
     args = [
         "terraform",
@@ -190,7 +251,9 @@ def destroy(cfg: Config, command_executor: CommandExecutor):
         "-auto-approve"
     ]
     cwd = os.path.join(BASE_DIR, "terraform", "aws_eks")
-    command_executor.execute(args=args, cwd=cwd)
+    exit_code = command_executor.execute(args=args, cwd=cwd)
+    if exit_code != 0:
+        raise Exception(f"Failed to destroy resources")
 
 
 def main():
--- s3blitzer/s3blitzer.py
+++ s3blitzer/s3blitzer.py
@@ -1,6 +1,8 @@
 import re
 import os
 import time
+import uuid
+import math
 import random
 import string
 import logging
@@ -9,6 +11,14 @@ import boto3
 from locust import task, between, User, events, SequentialTaskSet
 
 from locust_wrapper import method_wrapper
+
+FILE_SIZE_PATTERN = re.compile(r"(\d+)([BKMGT])")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def largest_power_of_10_less_than(X):
+    power_of_10 = math.floor(math.log10(X))
+    return 10 ** power_of_10
 
 
 def generate_random_string(size):
@@ -30,8 +40,9 @@ def generate_random_text_file(filepath, file_size_bytes, chunk_size_bytes=1024):
 
 
 class BotoClient:
-    def __init__(self, environment):
+    def __init__(self, environment, user_uuid):
         self.environment = environment
+        self.user_uuid = user_uuid
         self.s3_client = boto3.client(
             's3',
             endpoint_url=self.environment.parsed_options.endpoint_url,
@@ -59,55 +70,49 @@ class BotoClient:
     def download_file_from_s3(self, key, dst_file):
         self.s3_client.download_file(self.environment.parsed_options.bucket_name, key, dst_file)
 
-    @method_wrapper(
-        request_meta={
-            'name': 'list_file',
-            'type': 's3'
-        }
-    )
-    def list_file_from_s3(self):
-        self.s3_client.list_objects(self.environment.parsed_options.bucket_name)
-
 
 class S3TasksBoto3(SequentialTaskSet):
 
     def __init__(self, parent: "User"):
         super().__init__(parent)
         self.environment = parent.environment
+        self.user_uuid = parent.uuid
+
+        self.file_sizes = self.environment.parsed_options.file_sizes.strip()
+        self.file_sizes = [size.strip() for size in self.file_sizes.split(",")]
+
+        self.testfiles_dir = os.path.join(BASE_DIR, "testfiles")
+
 
     @task
     def upload_task(self):
-        logging.info("Start upload task")
-
-        file_size = self.environment.parsed_options.file_size.strip()
-        testfiles_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testfiles")
-        file_name = f"{file_size}.txt"
-        src_path = os.path.join(testfiles_dir, file_name)
-        logging.info(f"uploading file {src_path}")
-        self.client.upload_file_to_s3(src_path, f"{file_name}-{time.time_ns()}")
-        logging.info("Finished upload task")
+        logging.info(f"Start upload task {self.user_uuid}")
+        for file_size in self.file_sizes:
+            file_name = f"{file_size}.txt"
+            src_path = os.path.join(self.testfiles_dir, file_name)
+            logging.info(f"Uploading file {src_path}")
+            self.client.upload_file_to_s3(src_path, f"{file_name}.{self.user_uuid}")
+        logging.info(f"Finished upload task {self.user_uuid}")
 
     @task
     def download_task(self):
-        logging.info("Start download task")
-        file_sizes = self.environment.parsed_options.file_sizes
-        file_sizes = [_size.strip() for _size in file_sizes.split(",")]
-        testfiles_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testfiles")
-        for file_size in file_sizes:
-            file_name = f"{file_size}.txt"
-            dst_file = os.path.join(testfiles_dir, file_name + ".download")
+        logging.info(f"Start download task {self.user_uuid}")
+        for file_size in self.file_sizes:
+            file_name = f"{file_size}.txt.{self.user_uuid}"
+            dst_file = os.path.join(self.testfiles_dir, f"{file_name}.download")
             logging.info(f"downloading file {file_name}")
             self.client.download_file_from_s3(file_name, dst_file)
-        logging.info("Finished download task")
+        logging.info(f"Finished download task {self.user_uuid}")
 
 
 class S3LoadTestUser(User):
     tasks = {S3TasksBoto3}
-    wait_time = between(1, 5)
+    wait_time = between(1, 2)
 
     def __init__(self, environment):
         super().__init__(environment)
-        self.client = BotoClient(self.environment)
+        self.uuid = uuid.uuid4()
+        self.client = BotoClient(self.environment, self.uuid)
 
 
 @events.init_command_line_parser.add_listener
@@ -125,38 +130,47 @@ def _(parser):
                         help="AWS_SECRET_ACCESS_KEY")
     parser.add_argument("--session-token", type=str, is_secret=True, default="",
                         help="AWS_SESSION_TOKEN")
-    parser.add_argument("--file-size", type=str, default="10M", help="file size (\d+)([BKMGT])")
+    parser.add_argument("--file-sizes", type=str, default="1M,10M,100M",
+                        help="comma separate list offile size (\d+)([BKMGT])")
 
 
 @events.test_start.add_listener
 def _(environment, **kw):
-    print(environment.parsed_options.__dict__)
-    file_size = environment.parsed_options.file_size.strip()
-    testfiles_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testfiles")
-    file_size_pattern = re.compile(r"(\d+)([BKMGT])")
-    filepath = os.path.join(testfiles_dir, f"{file_size}.txt")
-    logging.info(f"Generating test file with size {file_size} at {filepath}")
-    m = file_size_pattern.match(file_size)
-    if m:
-        size_int = m.group(1)
-        size_unit = m.group(2)
-        size_int_bytes = int(size_int)
-        chunk_size_bytes = 1
-        match size_unit:
-            case "B":
-                chunk_size_bytes = 1
-            case "K":
-                chunk_size_bytes = 1024
-            case "M":
-                chunk_size_bytes = 1024 * 1024
-            case "G":
-                chunk_size_bytes = 1024 * 1024 * 1024
-            case "T":
-                chunk_size_bytes = 1024 * 1024 * 1024 * 1024
+    file_sizes = environment.parsed_options.file_sizes.strip()
+    file_sizes = [size.strip() for size in file_sizes.split(",")]
+    testfiles_dir = os.path.join(BASE_DIR, "testfiles")
+    for file_size in file_sizes:
+        filepath = os.path.join(testfiles_dir, f"{file_size}.txt")
+        logging.info(f"Generating test file with size {file_size} at {filepath}")
+        m = FILE_SIZE_PATTERN.match(file_size)
+        if m:
+            size_int = m.group(1)
+            size_unit = m.group(2)
+            size_int_bytes = int(size_int)
+            chunk_size_bytes = 1
+            match size_unit:
+                case "B":
+                    chunk_size_bytes = 1
+                case "K":
+                    chunk_size_bytes = 1024
+                case "M":
+                    chunk_size_bytes = 1024 * 1024
+                case "G":
+                    chunk_size_bytes = 1024 * 1024 * 1024
+                case "T":
+                    chunk_size_bytes = 1024 * 1024 * 1024 * 1024
 
-        size_int_bytes *= chunk_size_bytes
+            size_int_bytes *= chunk_size_bytes
+            pow = largest_power_of_10_less_than(size_int_bytes)
 
-        generate_random_text_file(filepath, size_int_bytes, chunk_size_bytes)
+            # if file exist and only less than few bytes as expected then skip creation
+            if os.path.exists(filepath):
+                current_size = os.stat(filepath).st_size
+                if abs(current_size-size_int_bytes) <= 100:
+                    logging.info(f"File with size {file_size} already exists at {filepath}")
+                    continue
+
+            generate_random_text_file(filepath, size_int_bytes, pow*chunk_size_bytes)
 
 
 @events.test_stop.add_listener
